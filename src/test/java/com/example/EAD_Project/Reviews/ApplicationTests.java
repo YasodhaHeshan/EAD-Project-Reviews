42a78b1a4f345ab7e680a4157838ae12c0c862d5
YasodhaHeshan <yasodhaheshan2002@gmail.com>
2024-11-28 21:11:23 +0530

    first commit

---
+++ src/test/java/com/example/EAD_Project/Reviews/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EAD_Project.Reviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
